50cc8898217296a024d0b3133cdddc60c005de81
ram <ram@gmail.com>
2025-06-09 19:53:36 +0530

    Task 1

---
+++ src/api/sample-content/routes/sample-content.ts
@@ -0,0 +1,7 @@
+/**
+ * sample-content router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::sample-content.sample-content');
---
+++ src/api/sample-content/services/sample-content.ts
@@ -0,0 +1,7 @@
+/**
+ * sample-content service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::sample-content.sample-content');
